e0abda4b5c432290653b0cb08a119dd5795cb1c6
JakubCiesko <jakub.ciesko@gmail.com>
2025-03-21 10:05:45 +0100

    added docstrings to extractor

--- extractors/extractor.py
+++ extractors/extractor.py
@@ -3,15 +3,37 @@ from selectolax.parser import Node
 from datetime import datetime
 
 class FlyerDataExtractor:
+    """Extracts flyer data from HTML nodes."""
+
     def __init__(self):
         pass
 
     def extract(self, fliers: list[Node], shop_name: str) -> list[FlyerData]:
+        """
+        Extracts flyer information from a list of HTML nodes.
+
+        Args:
+            fliers (list[Node]): A list of flyer HTML selectolax nodes.
+            shop_name (str): The name of the shop associated with the flyers.
+
+        Returns:
+            list[FlyerData]: A list of extracted flyer data.
+        """
         if isinstance(fliers, Node):
             fliers = [fliers]
         return [self._extract_flyer_info(flyer, shop_name) for flyer in fliers]
 
     def _extract_flyer_info(self, flyer: Node, shop_name:str) -> FlyerData:
+        """
+        Extracts relevant details from a single flyer HTML node.
+
+        Args:
+            flyer (Node): The HTML node containing flyer details.
+            shop_name (str): The shop name associated with the flyer.
+
+        Returns:
+            FlyerData: The extracted flyer data including title, thumbnail, validity dates, and parsed time.
+        """
         flyer_contents = flyer.css_first(".letak-description").css(".grid-item-content") 
         if flyer_contents:
             thumbnail_node = flyer.css_first("picture img")
@@ -30,7 +52,16 @@ class FlyerDataExtractor:
             valid_to=valid_to
         )
 
-    def _parse_dates(self, dates: list[str]) -> tuple[str, str]:
+    def _parse_dates(self, dates: str) -> tuple[str, str]:
+        """
+        Parses the start and end date from a date string.
+
+        Args:
+            dates (str): The date range string in the format "dd.mm. - dd.mm.yyyy".
+
+        Returns:
+            tuple[str, str]: A tuple containing ISO format start and end dates or the starting date only
+        """
         dates = dates.split("-")
         if len(dates) >= 2:
             start_date_raw, end_date_raw = dates[0].strip(), dates[1].strip()
@@ -42,12 +73,30 @@ class FlyerDataExtractor:
         return dates[0].strip(), ""
     
     def _get_thumbnail_src(self, thumbnail_node: Node) -> str:
+        """
+        Retrieves the thumbnail source URL from the flyer node.
+
+        Args:
+            thumbnail_node (Node): The HTML node containing the flyer thumbnail.
+
+        Returns:
+            str: The extracted thumbnail URL or an empty string if not found.
+        """
         thumbnail_src = thumbnail_node.attributes.get("src") # need to account for thumbnails of better quality
         thumbnail_data_src = thumbnail_node.attributes.get("data-src")
         src = thumbnail_src or thumbnail_data_src
         return src or ""
 
     def _polish_thumbnail_src(self, src: str) -> str: 
+        """
+        Cleans and improves the quality of the thumbnail URL.
+
+        Args:
+            src (str): The raw thumbnail URL.
+
+        Returns:
+            str: The improved thumbnail URL.
+        """
         if src:
             jpg_extension_index = src.find(".jpg")
             src = src[:jpg_extension_index] if jpg_extension_index >= 0 else src 
